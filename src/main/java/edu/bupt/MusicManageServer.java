f9b772e0e0ec7723040322b84c8af7bb93de9411
Goplush <15611129374@163.com>
2022-06-29 08:28:34 +0800

    update
    maven support
    add mongodb support

--- src/main/java/edu/bupt/MusicManageServer.java
+++ src/main/java/edu/bupt/MusicManageServer.java
@@ -1,5 +1,34 @@
 package edu.bupt;
 
-public class MusicManageServer {
+import redis.clients.jedis.Jedis;
 
+public class MusicManageServer {
+    private Jedis redis_link;
+
+    public MusicManageServer(){
+        redis_link = new Jedis("localhost", 6379);
+        redis_link.auth("K5nCjD$vwj8k^DeW");
+
+
+    }
+}
+class SongTokenAddRequest{
+    private String song_name, singer, new_token_name;
+    SongTokenAddRequest(String sname, String singer, String token_name){
+        song_name = sname;
+        singer = singer;
+        new_token_name = token_name;
+    }
+
+    public String getNew_token_name() {
+        return new_token_name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public String getSong_name() {
+        return song_name;
+    }
 }
